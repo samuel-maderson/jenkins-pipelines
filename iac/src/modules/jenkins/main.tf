cb960ba210f8b5123f305249e24797739e566f12
Samuel Leão <samuelleao@Samuels-MacBook-Pro.local>
2025-02-04 23:40:01 -0300

    fix gpg key

--- iac/src/modules/jenkins/main.tf
+++ iac/src/modules/jenkins/main.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "jenkins" {
               sudo yum update -y
               sudo yum install -y java-17-amazon-corretto
               wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-              rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+              rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
               sudo yum install -y jenkins
               sudo systemctl enable jenkins
               sudo systemctl start jenkins
