b9efd894a29b0b1445ab7194e6a8293dc11c8456
Samuel Leão <samuelleao@Samuels-MacBook-Pro.local>
2025-02-05 23:40:36 -0300

    fix jenkinsfile name

--- iac/src/modules/jenkins/main.tf
+++ iac/src/modules/jenkins/main.tf
@@ -39,6 +39,7 @@ resource "aws_instance" "jenkins" {
               #!/bin/bash
               sudo yum update -y
               sudo yum install -y java-17-amazon-corretto
+              sudo yum install -y git
               wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
               rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
               sudo yum install -y jenkins
